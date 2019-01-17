2ee1e1be283a132fd4f74fc35e499f7955fd8291
Alexander Simpson <as2388@cam.ac.uk>
2019-01-17 14:26:57 +0000

    Move set_channel_keys from auto_code_surveys -> auto_code_show_messages

--- project_redss/auto_code_show_messages.py
+++ project_redss/auto_code_show_messages.py
@@ -9,7 +9,7 @@ from core_data_modules.traced_data.io import TracedDataCSVIO, TracedDataCoda2IO
 from core_data_modules.util import IOUtils
 from dateutil.parser import isoparse
 
-from project_redss.lib import ICRTools
+from project_redss.lib import ICRTools, Channels
 from project_redss.lib import MessageFilters
 from project_redss.lib.pipeline_configuration import PipelineConfiguration
 
@@ -57,6 +57,9 @@ class AutoCodeShowMessages(object):
                     )
                     missing_dict[plan.coded_field] = [na_label.to_dict()]
             td.append_data(missing_dict, Metadata(user, Metadata.get_call_location(), time.time()))
+
+        # Label each message with channel keys
+        Channels.set_channel_keys(user, data, cls.SENT_ON_KEY)
 
         # Filter for messages which aren't noise (in order to export to Coda and export for ICR)
         not_noise = MessageFilters.filter_noise(data, cls.NOISE_KEY, lambda x: x)
