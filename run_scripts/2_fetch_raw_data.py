09d178473721a5761bea6972be54d9cb505204de
Alexander Simpson <as2388@cam.ac.uk>
2019-02-04 16:08:51 +0000

    Use an explicit path to the google cloud credentials file rather than an environment variable

--- run_scripts/2_fetch_raw_data.py
+++ run_scripts/2_fetch_raw_data.py
@@ -11,8 +11,11 @@ if __name__ == "__main__":
                                                  "This script must be run from its parent directory.")
 
     parser.add_argument("user", help="Identifier of the user launching this program")
+    parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
+                        help="Path to a Google Cloud service account credentials file to use to access the "
+                             "credentials bucket")
     parser.add_argument("pipeline_configuration_file_path", metavar="pipeline-configuration-file",
-                        help="Path to the pipeline configuration json file")
+                        help="Path to the pipeline configuration json file"),
     parser.add_argument("rapid_pro_tools_dir", metavar="rapid-pro-tools-dir",
                         help="Path to a directory to checkout the required version of RapidProTools to")
     parser.add_argument("root_data_dir", metavar="root-data-dir",
@@ -22,6 +25,7 @@ if __name__ == "__main__":
 
     user = args.user
     pipeline_configuration_file_path = args.pipeline_configuration_file_path
+    google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     rapid_pro_tools_dir = args.rapid_pro_tools_dir
     root_data_dir = os.path.abspath(args.root_data_dir)
 
@@ -59,11 +63,12 @@ if __name__ == "__main__":
     bucket_name = parsed_rapid_pro_token_file_url.netloc
     blob_name = parsed_rapid_pro_token_file_url.path.lstrip("/")
 
-    print(f"Downloading {blob_name} from bucket {bucket_name}...")
-    storage_client = storage.Client()
+    print(f"Downloading Rapid Pro token from file '{blob_name}' in bucket '{bucket_name}'...")
+    storage_client = storage.Client.from_service_account_json(google_cloud_credentials_file_path)
     credentials_bucket = storage_client.bucket(bucket_name)
     credentials_file = credentials_bucket.blob(blob_name)
     rapid_pro_token = credentials_file.download_as_string().strip()
+    print("Downloaded Rapid Pro token.")
 
     # Download all the runs for each of the radio shows
     for show in SHOWS:
