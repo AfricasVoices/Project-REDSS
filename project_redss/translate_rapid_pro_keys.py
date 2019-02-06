c203946cb8f0ab56857de4e41e86619ae5d145e6
Alexander Simpson <as2388@cam.ac.uk>
2019-02-06 14:59:19 +0000

    First pass at separating the radio show remapping from the key name remapping

--- project_redss/translate_rapid_pro_keys.py
+++ project_redss/translate_rapid_pro_keys.py
@@ -1,8 +1,8 @@
-import time
 from os import path
 
 from core_data_modules.traced_data import Metadata
 from core_data_modules.traced_data.io import TracedDataCoda2IO
+from core_data_modules.util import TimeUtils
 from dateutil.parser import isoparse
 
 from project_redss.lib.redss_schemes import CodeSchemes
@@ -13,11 +13,6 @@ class TranslateRapidProKeys(object):
         # List of (new_key, old_key)
         ("uid", "avf_phone_id"),
 
-        ("rqa_s01e01_raw", "Rqa_S01E01 (Value) - csap_s01e01_activation"),
-        ("rqa_s01e02_raw", "Rqa_S01E02 (Value) - csap_s01e02_activation"),
-        # Not setting weeks 3 or 4 key here because they contain some messages from the other weeks.
-        # Special handling is performed in cls.translate_rapid_pro_keys()
-
         ("rqa_s01e01_run_id", "Rqa_S01E01 (Run ID) - csap_s01e01_activation"),
         ("rqa_s01e02_run_id", "Rqa_S01E02 (Run ID) - csap_s01e02_activation"),
         ("rqa_s01e03_run_id", "Rqa_S01E03 (Run ID) - csap_s01e03_activation"),
@@ -26,6 +21,7 @@ class TranslateRapidProKeys(object):
         ("sent_on", "Rqa_S01E01 (Time) - csap_s01e01_activation"),
         ("sent_on", "Rqa_S01E02 (Time) - csap_s01e02_activation"),
         ("sent_on", "Rqa_S01E03 (Time) - csap_s01e03_activation"),
+        ("sent_on", "Rqa_S01E04 (Time) - csap_s01e04_activation"),
 
         ("gender_raw", "Gender (Value) - csap_demog"),
         ("gender_time", "Gender (Time) - csap_demog"),
@@ -46,9 +42,16 @@ class TranslateRapidProKeys(object):
         ("involved_time", "Involved (Time) - csap_evaluation")
     ]
 
+    SHOW_ID_MAP = {
+        "Rqa_S01E01 (Value) - csap_s01e01_activation": 1,
+        "Rqa_S01E02 (Value) - csap_s01e02_activation": 2,
+        "Rqa_S01E03 (Value) - csap_s01e03_activation": 3,
+        "Rqa_S01E04 (Value) - csap_s01e04_activation": 4
+    }
+
     WEEK_3_TIME_KEY = "Rqa_S01E03 (Time) - csap_s01e03_activation"
     WEEK_3_VALUE_KEY = "Rqa_S01E03 (Value) - csap_s01e03_activation"
-    WEEK_4_START = isoparse("2018-12-23T00:00:00+03:00")
+    WEEK_4_START = isoparse("2018-12-23T00:00:00+03:00")  # TODO: All other dates are strings, this should be too
 
     WEEK_4_TIME_KEY = "Rqa_S01E04 (Time) - csap_s01e04_activation"
     WEEK_4_VALUE_KEY = "Rqa_S01E04 (Value) - csap_s01e04_activation"
@@ -62,7 +65,7 @@ class TranslateRapidProKeys(object):
     FRIDAY_CORRECTION_TIME = "2018-12-14T00:00:00+03:00"
 
     @classmethod
-    def build_message_to_s01e02_dict(cls, user, data, coda_input_dir):
+    def _build_message_to_s01e02_dict(cls, user, data, coda_input_dir):
         # Duplicate the input list because reading the file requires appending data to the TracedData,
         # and we don't actually want to modify the input at this stage of the pipeline.
         data = [td.copy() for td in data]
@@ -87,59 +90,82 @@ class TranslateRapidProKeys(object):
         return message_to_ws_dict
 
     @classmethod
-    def translate_rapid_pro_keys(cls, user, data, coda_input_dir):
-        """
-        Uses the cls.RAPID_PRO_KEY_MAP to rename the keys exported by Rapid Pro to keys which are easier to work
-        with in the pipeline. 
-        
-        Also performs several project-specific redirects of radio show question messages which went into the wrong
-        activation flow when running the project. These redirects are described in the comments below. These
-        redirects are performed here so that the rest of the pipeline can be given a dataset where it looked like
-        nothing went wrong, which means operational corrections shouldn't be needed so much elsewhere.
-        """
-        
+    def set_show_ids(cls, user, data, show_id_map):
+        for td in data:
+            show_dict = dict()
+
+            for message_key, show_id in show_id_map.items():
+                if message_key in td:
+                    assert "rqa_message" not in show_dict
+                    show_dict["rqa_message"] = td[message_key]
+                    show_dict["show_id"] = show_id
+
+            td.append_data(show_dict, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
+
+    @classmethod
+    def remap_radio_show_weeks(cls, user, data, coda_input_dir):
         # Build a map of raw week 3 messages to wrong scheme data
-        message_to_s01e02_dict = cls.build_message_to_s01e02_dict(user, data, coda_input_dir)
-        
-        # Do the actual key mapping
+        message_to_s01e02_dict = cls._build_message_to_s01e02_dict(user, data, coda_input_dir)
+
         for td in data:
             mapped_dict = dict()
-            
+
             if cls.WEEK_3_TIME_KEY in td:
                 # Redirect any week 3 messages coded as s01e02 in the WS - Correct Dataset scheme to week 2
-                if message_to_s01e02_dict.get(td[cls.WEEK_3_VALUE_KEY], False):
-                    mapped_dict["rqa_s01e02_raw"] = td[cls.WEEK_3_VALUE_KEY]
+                if message_to_s01e02_dict.get(td["rqa_message"], False):
+                    mapped_dict["show_id"] = 2
                 # Redirect any week 4 messages which were in the week 3 flow due to a late flow change-over.
                 elif isoparse(td[cls.WEEK_3_TIME_KEY]) > cls.WEEK_4_START:
-                    mapped_dict["rqa_s01e04_raw"] = td[cls.WEEK_3_VALUE_KEY]
-                else:
-                    mapped_dict["rqa_s01e03_raw"] = td[cls.WEEK_3_VALUE_KEY]
+                    mapped_dict["show_id"] = 4
 
             # Redirect any week 2 messages which were in the week 4 flow, due to undelivered messages being delivered
             # in two bursts after the end of the radio shows.
             if cls.WEEK_4_TIME_KEY in td:
                 if isoparse(cls.THURSDAY_BURST_START) <= isoparse(td[cls.WEEK_4_TIME_KEY]) < isoparse(cls.THURSDAY_BURST_END):
-                    mapped_dict["rqa_s01e02_raw"] = td[cls.WEEK_4_VALUE_KEY]
+                    mapped_dict["show_id"] = 2
                     mapped_dict["sent_on"] = cls.THURSDAY_CORRECTION_TIME
                 elif isoparse(cls.FRIDAY_BURST_START) <= isoparse(td[cls.WEEK_4_TIME_KEY]) < isoparse(cls.FRIDAY_BURST_END):
-                    mapped_dict["rqa_s01e02_raw"] = td[cls.WEEK_4_VALUE_KEY]
+                    mapped_dict["show_id"] = 2
                     mapped_dict["sent_on"] = cls.FRIDAY_CORRECTION_TIME
-                else:
-                    mapped_dict["rqa_s01e04_raw"] = td[cls.WEEK_4_VALUE_KEY]
-                    mapped_dict["sent_on"] = td[cls.WEEK_4_TIME_KEY]
 
-            # Translate all other keys
-            for new_key, old_key in cls.RAPID_PRO_KEY_MAP:
-                if old_key in td:
-                    mapped_dict[new_key] = td[old_key]
+            # Fake the timestamp of redirected week 3 messages to make it look like they arrived on the day
+            # before the incorrect sms ad was sent, i.e. the last day of week 2.
+            # This is super yucky, but works because (a) timestamps are never exported, and (b) this date
+            # is being set to non_logical anyway in channels.py.
+            if cls.WEEK_3_TIME_KEY in td:
+                if message_to_s01e02_dict.get(td["rqa_message"], False):
+                    mapped_dict["sent_on"] = "2018-12-15T00:00:00+03:00"
 
-            if cls.WEEK_3_TIME_KEY in td and message_to_s01e02_dict.get(td[cls.WEEK_3_VALUE_KEY], False):
-                # Fake the timestamp of redirected week 3 messages to make it look like they arrived on the day
-                # before the incorrect sms ad was sent, i.e. the last day of week 2.
-                # This is super yucky, but works because (a) timestamps are never exported, and (b) this date
-                # is being set to non_logical anyway in channels.py.
-                mapped_dict["sent_on"] = "2018-12-15T00:00:00+03:00"
+            td.append_data(mapped_dict, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
 
-            td.append_data(mapped_dict, Metadata(user, Metadata.get_call_location(), time.time()))
+    @classmethod
+    def remap_key_names(cls, user, data, key_map):
+        for td in data:
+            remapped = dict()
+               
+            for new_key, old_key in key_map:
+                if old_key in td and new_key not in td:
+                    remapped[new_key] = td[old_key]
+
+            td.append_data(remapped, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
+
+    @classmethod
+    def translate_rapid_pro_keys(cls, user, data, coda_input_dir):
+        cls.set_show_ids(user, data, cls.SHOW_ID_MAP)
+        cls.remap_radio_show_weeks(user, data, coda_input_dir)
+        cls.remap_key_names(user, data, cls.RAPID_PRO_KEY_MAP)
+
+        raw_id_map = {
+            1: "rqa_s01e01_raw",
+            2: "rqa_s01e02_raw",
+            3: "rqa_s01e03_raw",
+            4: "rqa_s01e04_raw"
+        }
+
+        for td in data:
+            for show_id, message_key in raw_id_map.items():
+                if "rqa_message" in td and td.get("show_id") == show_id:
+                    td.append_data({message_key: td["rqa_message"]},
+                                   Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
 
         return data
