c7519f29e7f381e007d52d5ea8e7cd9a6440e51d
Alexander Simpson <as2388@cam.ac.uk>
2019-02-06 16:05:17 +0000

    Add docstrings

--- project_redss/translate_rapid_pro_keys.py
+++ project_redss/translate_rapid_pro_keys.py
@@ -9,6 +9,7 @@ from project_redss.lib.redss_schemes import CodeSchemes
 
 
 class TranslateRapidProKeys(object):
+    # TODO: Move the constants in this file to configuration json
     RAPID_PRO_KEY_MAP = [
         # List of (new_key, old_key)
         ("uid", "avf_phone_id"),
@@ -98,6 +99,17 @@ class TranslateRapidProKeys(object):
 
     @classmethod
     def set_show_ids(cls, user, data, show_id_map):
+        """
+        Sets a show_id for each message, using the presence of Rapid Pro value keys to determine which show each message
+        belongs to.
+
+        :param user: Identifier of the user running this program, for TracedData Metadata.
+        :type user: str
+        :param data: TracedData objects to set the show ids of.
+        :type data: iterable of TracedData
+        :param show_id_map: Dictionary of Rapid Pro value key to show id.
+        :type show_id_map: dict of str -> int
+        """
         for td in data:
             show_dict = dict()
 
@@ -110,7 +122,22 @@ class TranslateRapidProKeys(object):
             td.append_data(show_dict, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
 
     @classmethod
-    def remap_radio_show_weeks(cls, user, data, coda_input_dir):
+    def remap_radio_shows(cls, user, data, coda_input_dir):
+        """
+        Remaps radio shows which were in the wrong flow, and therefore have the wrong key/values set, to have the
+        key/values they would have had if they had been received by the correct flow.
+
+        :param user: Identifier of the user running this program, for TracedData Metadata.
+        :type user: str
+        :param data: TracedData objects to move the radio show messages in.
+        :type data: iterable of TracedData
+        :param coda_input_dir: Directory to read coded coda files from.
+        :type coda_input_dir: str
+        """
+        # TODO: Convert the show remapping code here into reusable functions for each case that they handle.
+        #       Note that ultimately we probably don't want to handle the 'WS' show remapping here,
+        #       because we get that for free when we implement 'WS' handling properly.
+
         # Build a map of raw week 3 messages to wrong scheme data
         message_to_s01e02_dict = cls._build_message_to_s01e02_dict(user, data, coda_input_dir)
 
@@ -145,6 +172,16 @@ class TranslateRapidProKeys(object):
 
     @classmethod
     def remap_key_names(cls, user, data, key_map):
+        """
+        Remaps key names.
+        
+        :param user: Identifier of the user running this program, for TracedData Metadata.
+        :type user: str
+        :param data: TracedData objects to remap the key names of.
+        :type data: iterable of TracedData
+        :param key_map: Iterable of (new_key, old_key).
+        :type key_map: iterable of (str, str)
+        """
         for td in data:
             remapped = dict()
                
@@ -156,6 +193,21 @@ class TranslateRapidProKeys(object):
 
     @classmethod
     def set_rqa_raw_keys_from_show_ids(cls, user, data, raw_id_map):
+        """
+        Despite the earlier phases of this pipeline stage using a common 'rqa_message' field and then a 'show_id'
+        field to identify which radio show a message belonged to, the rest of the pipeline still uses the presence
+        of a raw field for each show to determine which show a message belongs to. This function translates from
+        the new 'show_id' method back to the old 'raw field presence` method.
+        
+        TODO: Update the rest of the pipeline to use show_ids, and/or perform remapping before combining the datasets.
+
+        :param user: Identifier of the user running this program, for TracedData Metadata.
+        :type user: str
+        :param data: TracedData objects to set raw radio show message fields for.
+        :type data: iterable of TracedData
+        :param raw_id_map: Dictionary of show id to the rqa message key to assign each td[rqa_message} to.
+        :type raw_id_map: dict of int -> str
+        """
         for td in data:
             for show_id, message_key in raw_id_map.items():
                 if "rqa_message" in td and td.get("show_id") == show_id:
@@ -164,9 +216,25 @@ class TranslateRapidProKeys(object):
 
     @classmethod
     def translate_rapid_pro_keys(cls, user, data, coda_input_dir):
+        """
+        Remaps the keys of rqa messages in the wrong flow into the correct one, and remaps all Rapid Pro keys to
+        more usable keys that can be used by the rest of the pipeline.
+        
+        TODO: Break this function such that the show remapping phase happens in one class, and the Rapid Pro remapping
+              in another?
+        """
+        # Set a show id field for each message, using the presence of Rapid Pro value keys in the TracedData.
+        # Show ids are necessary in order to be able to remap radio shows and key names separately (because data
+        # can't be 'deleted' from TracedData).
         cls.set_show_ids(user, data, cls.SHOW_ID_MAP)
-        cls.remap_radio_show_weeks(user, data, coda_input_dir)
+
+        # Move rqa messages which ended up in the wrong flow to the correct one.
+        cls.remap_radio_shows(user, data, coda_input_dir)
+
+        # Remap the keys used by Rapid Pro to more usable key names that will be used by the rest of the pipeline.
         cls.remap_key_names(user, data, cls.RAPID_PRO_KEY_MAP)
+
+        # Convert from the new show id format to the raw field format still used by the rest of the pipeline.
         cls.set_rqa_raw_keys_from_show_ids(user, data, cls.RAW_ID_MAP)
 
         return data
